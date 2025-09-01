9c30b352ef95318d823d179c3d6b1366bb606d2c
Aleksander Desenko <aleksanderdesenko@MacBook-Air-Aleksander.local>
2025-09-01 22:41:16 +0300

    room creating eindpoints v.1

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const db = require('./db/models');
 const authRoutes = require('./src/routes/auth.routes');
 const userRouter = require('./src/routes/user.routes');
+const roomRouter = require('./src/routes/room.routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -23,6 +24,7 @@ app.use(cors({
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRouter);
+app.use('/api/room', roomRouter);
 
 // Глобальный обработчик ошибок
 app.use((err, req, res, next) => {
---
+++ server/src/routes/room.routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const {Game_sessions} = require('../../db/models')
+
+// создать новую комнату
+
+roomRouter.post("/new", async (req, res) => {
+    try {
+      const { phase_id, current_topic_id, current_question_id, room_code, is_active, room_mame } = req.body;
+      
+      if (!phase_id || !current_topic_id || !current_question_id || !room_code || !is_active || !room_mame) {
+        return res.status(400).send("Не все обязательные поля переданы с клиента");
+      }
+      
+      const room = await Game_sessions.create({ phase_id, current_topic_id, current_question_id, room_code, is_active, room_mame });
+  
+      res.status(201).json({ message: "success", data: room });
+    } catch (error) {
+      console.log("Ошибка на сервере:", error);
+      res.status(500).json({ message: "Ошибка сервера", error });
+    }
+  });
+
+module.exports = roomRouter;
