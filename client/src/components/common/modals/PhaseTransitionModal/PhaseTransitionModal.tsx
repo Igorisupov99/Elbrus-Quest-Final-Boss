b512b00575ac05f362c1fc619ede2337364cb57c
dmitry-js-p <dmitryshes@icloud.com>
2025-09-17 19:04:52 +0300

    fix

--- client/src/components/common/modals/PhaseTransitionModal/PhaseTransitionModal.tsx
+++ client/src/components/common/modals/PhaseTransitionModal/PhaseTransitionModal.tsx
@@ -41,7 +41,7 @@ export default function PhaseTransitionModal({
             Экзамен успешно сдан!
           </p>
           <p className={styles.phaseMessage}>
-            Вы переходите на следующую фазу
+            Вы переходите на фазу {phaseNumber}
           </p>
           <p className={styles.rewardMessage}>
             Каждый игрок получил <span className={styles.rewardPoints}>+{rewardPoints} очков</span>
